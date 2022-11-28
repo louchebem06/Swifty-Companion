61ca723cbec2a648d2f85d634dd783e42678b952
bledda <bledda@student.42nice.fr>
2022-11-28 20:16:10 +0100

    Initial Commit

---
+++ swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Bryan Ledda on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
