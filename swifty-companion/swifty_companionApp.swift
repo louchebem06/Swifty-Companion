1243f9e4cabd3e130ae81c6ea6c01277b2782797
bledda <bledda@student.42nice.fr>
2022-11-28 21:16:02 +0100

    UPDATE: first commit

--- swifty-companion/swifty_companionApp.swift
+++ swifty-companion/swifty_companionApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct swifty_companionApp: App {
+    init () {
+        let UID_42: String = ProcessInfo.processInfo.environment["UID_42"] ?? "";
+        let SECRET_42: String = ProcessInfo.processInfo.environment["SECRET_42"] ?? "";
+        if (UID_42.isEmpty || SECRET_42.isEmpty) {
+            print("Env not set");
+            exit(1);
+        }
+        print(UID_42)
+        print(SECRET_42);
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
