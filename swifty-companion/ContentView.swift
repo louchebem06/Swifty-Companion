82c10039a42e732994aaa527e7892b1b028f989f
bledda <bledda@student.42nice.fr>
2022-11-29 01:33:53 +0100

    UPDATE: clean code

--- swifty-companion/ContentView.swift
+++ swifty-companion/ContentView.swift
@@ -9,21 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.openURL) private var openURL;
-    let UID_42: String = ProcessInfo.processInfo.environment["UID_42"] ?? "";
-    let SECRET_42: String = ProcessInfo.processInfo.environment["SECRET_42"] ?? "";
     let authUrl: String;
-
-    init () {
-        if (UID_42.isEmpty || SECRET_42.isEmpty) {
-            print("Env not set");
-            exit(1);
-        }
-        print(UID_42)
-        print(SECRET_42);
-        let redirect: String = "&redirect_uri=swifty-companion%3A%2F%2Fauth&response_type=code";
-        let api42Url: String = "https://api.intra.42.fr";
-        authUrl = api42Url + "/oauth/authorize?client_id=" + UID_42 + redirect;
-    }
     
     var body: some View {
         Button {
@@ -38,6 +24,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(authUrl: "")
     }
 }
--- swifty-companion/swifty_companionApp.swift
+++ swifty-companion/swifty_companionApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct swifty_companionApp: App {
+    let UID_42: String = ProcessInfo.processInfo.environment["UID_42"] ?? "";
+    let SECRET_42: String = ProcessInfo.processInfo.environment["SECRET_42"] ?? "";
+    let authUrl: String;
+
+    init () {
+        if (UID_42.isEmpty || SECRET_42.isEmpty) {
+            print("Env not set");
+            exit(1);
+        }
+        let api42Url: String = "https://api.intra.42.fr";
+        let redirect: String = "&redirect_uri=swifty-companion%3A%2F%2Fauth&response_type=code";
+        authUrl = api42Url + "/oauth/authorize?client_id=" + UID_42 + redirect;
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(authUrl: authUrl)
         }
     }
 }
