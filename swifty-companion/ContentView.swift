81c596ab55719c85bae0668e8deaabb7fb8f01f7
bledda <bledda@student.42nice.fr>
2022-11-29 01:12:34 +0100

    UPDATE: safari open app

--- swifty-companion/ContentView.swift
+++ swifty-companion/ContentView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.openURL) private var openURL;
+    let UID_42: String = ProcessInfo.processInfo.environment["UID_42"] ?? "";
+    let SECRET_42: String = ProcessInfo.processInfo.environment["SECRET_42"] ?? "";
+    let authUrl: String;
+
+    init () {
+        if (UID_42.isEmpty || SECRET_42.isEmpty) {
+            print("Env not set");
+            exit(1);
+        }
+        print(UID_42)
+        print(SECRET_42);
+        let redirect: String = "&redirect_uri=swifty-companion%3A%2F%2Fauth&response_type=code";
+        let api42Url: String = "https://api.intra.42.fr";
+        authUrl = api42Url + "/oauth/authorize?client_id=" + UID_42 + redirect;
+    }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            if let url = URL(string: authUrl) {
+                openURL(url);
+            }
+        } label: {
+            Label("Intra 42", systemImage: "terminal")
         }
-        .padding()
     }
 }
 
