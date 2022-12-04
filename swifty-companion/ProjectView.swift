0714ed38c706cb6aec99410094dc817b536f7d74
bledda <bledda@student.42nice.fr>
2022-12-04 22:26:44 +0100

    Update ProjectView.swift

--- swifty-companion/ProjectView.swift
+++ swifty-companion/ProjectView.swift
@@ -23,54 +23,45 @@ struct ProjectView: View {
 	var values: [ProjectsItem] = [];
 	
 	init(_ projects: [ProjectsUser?], _ cursusId: Int) {
+		getMasterProjects(projects, cursusId);
+		getChildProjects(projects, cursusId);
+	}
+	
+	func getProjectsItem(_ project: ProjectsUser) -> ProjectsItem {
+		return (ProjectsItem(
+			id42: project.project.id,
+			occurrence: project.occurrence,
+			name: project.project.name,
+			final_mark: project.final_mark,
+			status: project.status,
+			validated: project.validated,
+			parent_id: project.project.parent_id
+		));
+	}
+	
+	mutating func getMasterProjects(_ projects: [ProjectsUser?], _ cursusId: Int) -> Void {
 		for project in projects {
 			if (project?.project.parent_id != nil) {
 				continue ;
 			}
-			let id = project!.project.id;
-			let name = project!.project.name;
-			let occurrence = project!.occurrence;
-			let final_mark = project?.final_mark;
-			let status = project!.status;
-			let validated = project?.validated;
-			let item = ProjectsItem(
-				id42: id,
-				occurrence: occurrence,
-				name: name,
-				final_mark: final_mark,
-				status: status,
-				validated: validated
-			);
 			for id in project!.cursus_ids {
 				if (id == cursusId) {
-					values.append(item);
+					values.append(getProjectsItem(project!));
 					break ;
 				}
 			}
 		}
+	}
+	
+	mutating func getChildProjects(_ projects: [ProjectsUser?], _ cursusId: Int) -> Void {
 		for project in projects {
 			if (project?.project.parent_id == nil) {
 				continue ;
 			}
-			let id = project!.project.id;
-			let name = project!.project.name;
-			let occurrence = project!.occurrence;
-			let final_mark = project?.final_mark;
-			let status = project!.status;
-			let parent_id = project?.project.parent_id!;
-			let validated = project?.validated;
-			let item = ProjectsItem(
-				id42: id,
-				occurrence: occurrence,
-				name: name,
-				final_mark: final_mark,
-				status: status,
-				validated: validated,
-				parent_id: parent_id
-			);
 			for id in project!.cursus_ids {
 				if (id == cursusId) {
 					for i in 0..<values.count {
+						let item: ProjectsItem = getProjectsItem(project!);
 						if (values[i].id42 == item.parent_id) {
 							if (values[i].child == nil) {
 								values[i].child = [];
@@ -84,58 +75,34 @@ struct ProjectView: View {
 			}
 		}
 	}
-	
-	func getChild(_ item: ProjectsItem) -> some View {
-		HStack {
-			Text("\t - \(item.name)");
-			if (item.status == "finished") {
-				Text((item.validated ?? false) ? "✅" : "❌");
-				Text("\(String(item.final_mark!))/100");
-			} else {
-				Text(item.status);
-			}
-			Spacer();
-		}
-	}
-	
-	func getItem(_ item: ProjectsItem) -> some View {
-		VStack {
-			HStack {
-				Text("\(item.name)");
-				if (item.status == "finished") {
-					Text((item.validated ?? false) ? "✅" : "❌");
-					if (item.validated ?? false) {
-						if (item.occurrence == 0) {
-							Text("First try");
-						} else {
-							Text("In \(item.occurrence + 1) try")
-						}
-					}
-					Text("\(String(item.final_mark!))/100");
-				} else {
-					Text(item.status.replacingOccurrences(of: "_", with: " ").capitalized);
-				}
-				Spacer();
-			}
-			if (item.child != nil) {
-				VStack(alignment: .leading) {
-					ForEach(item.child!) {currentChild in
-						getChild(currentChild);
-					}
-				}
-			}
-		}
-	}
 
     var body: some View {
 		if (values.isEmpty) {
 			Text("Project not found")
 		} else {
-			VStack(alignment: .leading, spacing: 20) {
-				ForEach(values) {value in
-					getItem(value);
-				}
-			}.padding(10);
+			NavigationView {
+				List (values) { value in
+					HStack {
+						Text("\(value.name)");
+						if (value.status == "finished") {
+							Text((value.validated ?? false) ? "✅" : "❌");
+							if (value.validated ?? false) {
+								if (value.occurrence == 0) {
+									Text("First try");
+								} else {
+									Text("In \(value.occurrence + 1) try")
+								}
+							}
+							Text("\(String(value.final_mark!))/100");
+						} else {
+							Text(value.status.replacingOccurrences(of: "_", with: " ").capitalized);
+						}
+						if (value.child != nil) {
+							Text("Subproject: \(String(value.child!.count))")
+						}
+					}
+				}.navigationTitle("Projects");
+			}
 		}
 	}
 }
